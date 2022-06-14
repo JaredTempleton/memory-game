22e72e9811d6f540684cf13c57876fad462c70f3
JaredTempleton <95387966+JaredTempleton@users.noreply.github.com>
2022-06-14 16:39:11 -0400

    updated dependencies so api now hits correctly

--- src/hooks/useGetImages.js
+++ src/hooks/useGetImages.js
@@ -1,27 +1,35 @@
-const BASE_URL = 'https://api.pexels.com/v1/search';
+import { useEffect } from "react";
 
-const getRandomPage = () => Math.round(Math.random() * (10-1) + 1);
+const getRandomPage = () => Math.round(Math.random() * (10 - 1) + 1);
 
 const useGetImages = () => {
     const buildUrl = () => {
-        let url = new URL("https://api.pexels.com/v1/search");
+        let url = new URL('https://api.pexels.com/v1/search');
 
         url.search = new URLSearchParams({
-            query: "texture", //make variable
-            orientation: "square",
-            size: "small",
+            query: 'nature', //make variable
+            orientation: 'square',
+            size: 'small',
             per_page: 2, //make variable
             page: getRandomPage(),
         });
 
-        return url
+        return url;
     };
 
-    fetch(buildUrl, {
-        headers: {
-            Authorization: process.env.REACT_APP_AUTH_KEY,
-        },
-    });
+    const fetchPics = () => {
+        fetch(buildUrl(), {
+            headers: {
+                Authorization: process.env.REACT_APP_AUTH_KEY,
+            },
+        })
+            .then(data => data.json())
+            .then(data => console.log(data));
+    };
+
+    useEffect(() => {
+        fetchPics();
+    }, []);
 };
 
 export default useGetImages;
