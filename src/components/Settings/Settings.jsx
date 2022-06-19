15bca572f20a15934b0c0a9ce64b201a4aed8872
JaredTempleton <95387966+JaredTempleton@users.noreply.github.com>
2022-06-18 23:55:54 -0400

    logic for categories

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
+import { CATEGORIES } from '../../constants';
+
 import styles from './Settings.module.css';
+import RadioBox from '../RadioBox/RadioBox';
 
 const Settings = () => {
+    const [category, setCategory] = useState(CATEGORIES[0])
     return (
         <div className={`${styles.settings} frosted`}>
             <h2>Settings</h2>
+
             <h4>Category:</h4>
-            <div className={`${styles.settings}`}>Something</div>
+            <div className={`${styles.setting}`}>
+                {CATEGORIES.map(item => (
+                    <RadioBox key={item} name={item} 
+                    selectedItem={category} />
+                ))}
+            </div>
         </div>
     );
 };
